45cb47e9a5cf8e2a22671dc15970385cc828f255
techank802 <ankurpokharel2016@gmail.com>
2024-02-25 09:39:51 +0545

    ecommerce website Ankur

---
+++ src/products.js
@@ -0,0 +1,67 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+import product9 from "./assets/products/9.jpg";
+import product10 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Smart Watches",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Brad Thor Black Ice",
+    price: 199.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Laptop",
+    price: 1699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "The ChatGpt Millionare",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Anti Virus Protection",
+    price: 109.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "WinZip Pro",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Gaming Earbuds",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Motorolla Smartphone",
+    price: 440.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Camera Lenses for HD Photograph",
+    price: 45.0,
+    productImage: product9,
+  },
+];
